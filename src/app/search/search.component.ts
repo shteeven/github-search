7a0d0b63a6f6e8b8b796f2a91769924ea9ae00b8
Steven Barnhurst <stevenbarnhurst@gmail.com>
2022-06-10 16:23:53 +0200

    Wires up search input to api query

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { SearchApiService } from './search-api.service';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, map, of, switchMap } from 'rxjs';
 
 @Component({
   templateUrl: 'search.component.html'
 })
 export class SearchComponent {
-  results$ = this.api.getList({ query: 'steven' });
   searchValueSource = new BehaviorSubject('');
-  searchValue$ = this.searchValueSource.asObservable();
+  searchValue$ = this.searchValueSource
+    .asObservable()
+    .pipe(map((value) => value?.trim()));
+
+  results$ = this.searchValue$.pipe(
+    switchMap((value) => {
+      if (!value) return of(null);
+      return this.api.getList({ query: value });
+    })
+  );
 
   constructor(private api: SearchApiService) {}
 
