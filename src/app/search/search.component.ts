807c9aa739159e87c7b1676d703acf38e67d9598
Steven Barnhurst <stevenbarnhurst@gmail.com>
2022-06-10 16:07:33 +0200

    Adds search field to search page

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { SearchApiService } from './search-api.service';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
-  template: ` {{ results$ | async | json }} `
+  templateUrl: 'search.component.html'
 })
 export class SearchComponent {
   results$ = this.api.getList({ query: 'steven' });
+  searchValueSource = new BehaviorSubject('');
+  searchValue$ = this.searchValueSource.asObservable();
+
   constructor(private api: SearchApiService) {}
+
+  updateSearchValue(value: string) {
+    this.searchValueSource.next(value);
+  }
 }
