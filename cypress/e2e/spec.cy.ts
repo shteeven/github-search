9fb7037d020679efb6b3e4d8fd577073516cf8f8
Steven Barnhurst <stevenbarnhurst@gmail.com>
2022-06-12 13:08:02 +0200

    Adds cypress tests for search
    Adds error and no-input messages

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,7 +1,36 @@
-describe('My First Test', () => {
-  it('Visits the initial project page', () => {
-    cy.visit('/')
-    cy.contains('Welcome')
-    cy.contains('github-search app is running!')
-  })
-})
+import { QueryItem } from '../../src/app/search/search.models';
+
+describe('Search page', () => {
+  it('should show correct messages or results based on search value or response', () => {
+    cy.visit('/');
+    // Check initial state
+    cy.get('[data-test=searchInput]').should('be.visible');
+    cy.get('[data-test=resultContent]').should(
+      'contain.text',
+      'Enter a search'
+    );
+
+    // Should show no results message when nothing is returned
+    cy.intercept(
+      'GET',
+      'https://api.github.com/search/repositories**',
+      (req) => {
+        req.reply({
+          total_count: 0,
+          items: []
+        });
+      }
+    ).as('apiQuery');
+
+    cy.get('[data-test=searchInput]').type('random{enter}');
+    cy.wait('@apiQuery');
+    cy.get('[data-test=resultContent]').should('contain.text', ' No results');
+
+    // Should show initial message when search input is cleared
+    cy.get('[data-test=searchInput]').clear().type('{enter}');
+    cy.get('[data-test=resultContent]').should(
+      'contain.text',
+      'Enter a search'
+    );
+  });
+});
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,6 +1,15 @@
 import { Component } from '@angular/core';
 import { SearchApiService } from './search-api.service';
-import { BehaviorSubject, map, of, switchMap } from 'rxjs';
+import {
+  BehaviorSubject,
+  map,
+  Observable,
+  of,
+  pluck,
+  shareReplay,
+  switchMap
+} from 'rxjs';
+import { QueryItem } from './search.models';
 
 @Component({
   templateUrl: 'search.component.html'
@@ -15,8 +24,15 @@ export class SearchComponent {
     switchMap((value) => {
       if (!value) return of(null);
       return this.api.getList({ query: value });
+    }),
+    shareReplay({
+      refCount: true,
+      bufferSize: 1
     })
   );
+
+  items$ = this.results$.pipe(map((result) => result?.items));
+  count$ = this.results$.pipe(map((result) => result?.total_count));
 
   constructor(private api: SearchApiService) {}
 
